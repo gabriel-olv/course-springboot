9eedbfd577fb0a3ddd958d716a0e645178ca6032
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2022-08-25 00:27:52 -0300

    Project created

---
+++ src/main/java/br/com/gabrieldeoliveira/udemycoursejpoo/UdemycoursejpooApplication.java
@@ -0,0 +1,12 @@
+package br.com.gabrieldeoliveira.udemycoursejpoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemycoursejpooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemycoursejpooApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/gabrieldeoliveira/udemycoursejpoo/UdemycoursejpooApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrieldeoliveira.udemycoursejpoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemycoursejpooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
